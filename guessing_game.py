a6a30680dab507451e700f929e5b46a8914d8656
sradms0 <sradms0@openmailbox.org>
2019-07-15 08:30:57 -0400

    add guess prompt and check

--- guessing_game.py
+++ guessing_game.py
@@ -5,4 +5,9 @@ from random import randint
 def start_game():
     print("***Number Guessing Game***")
 
+    random_number = randint(1, 50)
+    while True:
+        user_number = int(input("Guess a number: "))
+        if user_number == random_number: break
+
 if __name__ == '__main__': start_game()
