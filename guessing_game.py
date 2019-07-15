48c9a252b2eafb3ec8f839cba7fb48996d8f797f
sradms0 <sradms0@openmailbox.org>
2019-07-15 10:32:38 -0400

    add comments

--- guessing_game.py
+++ guessing_game.py
@@ -16,24 +16,28 @@ def start_game():
         try:
             user_number = int(input("Guess a number: "))
 
+            # validate numeric input
             if user_number < _min or user_number > _max: 
                 print("Input is out of range\n")
                 continue
-
+            # provide hints
             elif user_number > random_number: print("It's lower")
             elif user_number < random_number: print("It's higher")
             guesses += 1
 
             elif user_number == random_number: 
+                # notify user of win and ask to play again
                 print("You got it! The number was {}".format(random_number))
                 print("It took you {} guess(es)".format(guesses))
                 play_again = input("Play again? [Y/N]: ").lower()
                 play_count += 1
 
                 if play_again == 'y': 
+                    # set best score to guesses on first game
                     if play_count == 1: best_score = guesses
                     elif guesses < best_score: best_score = guesses
                         
+                    # reset variables and display best score
                     random_number = randint(_min, _max)
                     user_number = 0
                     guesses = 0
