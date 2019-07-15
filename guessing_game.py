9d1a896d6ce5bc2ba74bb470937a7e28ebdec601
sradms0 <sradms0@openmailbox.org>
2019-07-15 10:59:17 -0400

    re-arrange input checks and add clear_screen function

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from random import randint
+import os
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 
 def start_game():
     print("***Number Guessing Game***")
@@ -13,9 +17,38 @@ def start_game():
     play_count = 0
 
     while random_number != user_number:
-        try:
-            user_number = int(input("Guess a number: "))
+        # disallow conversion crashing
+        try: user_number = int(input("Guess a number: "))
+        except ValueError: 
+            print("Only integers are allowed\n")
+            continue
+        
+        clear_screen()
+        guesses += 1
+        # user guessed right
+        if user_number == random_number: 
+            # notify user of win and ask to play again
+            print("You got it! The number was {}".format(random_number))
+            print("It took you {} guess(es)".format(guesses))
+            play_again = input("Play again? [Y/N]: ").lower()
+            play_count += 1
 
+            if play_again == 'y': 
+                # set best score to guesses on first game
+                if play_count == 1: best_score = guesses
+                elif guesses < best_score: best_score = guesses
+                    
+                # reset variables and display best score
+                random_number = randint(_min, _max)
+                user_number = 0
+                guesses = 0
+                print("Best score: {}".format(best_score))
+                print()
+                continue
+            else: print("\nThanks for playing!")
+
+        # check input further
+        else:
             # validate numeric input
             if user_number < _min or user_number > _max: 
                 print("Input is out of range\n")
@@ -23,31 +56,5 @@ def start_game():
             # provide hints
             elif user_number > random_number: print("It's lower")
             elif user_number < random_number: print("It's higher")
-            guesses += 1
-
-            if user_number == random_number: 
-                # notify user of win and ask to play again
-                print("You got it! The number was {}".format(random_number))
-                print("It took you {} guess(es)".format(guesses))
-                play_again = input("Play again? [Y/N]: ").lower()
-                play_count += 1
-
-                if play_again == 'y': 
-                    # set best score to guesses on first game
-                    if play_count == 1: best_score = guesses
-                    elif guesses < best_score: best_score = guesses
-                        
-                    # reset variables and display best score
-                    random_number = randint(_min, _max)
-                    user_number = 0
-                    guesses = 0
-                    print("Best score: {}".format(best_score))
-                    print()
-                    continue
-                else: print("\nThanks for playing!")
-        except ValueError: 
-            print("Only integers are allowed\n")
-            continue
-        print()
 
 if __name__ == '__main__': start_game()
