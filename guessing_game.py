d7f6d5d03b0559408c1bcddfae1f65ac39eaaa21
sradms0 <sradms0@openmailbox.org>
2019-07-15 10:11:50 -0400

    refactor and add replay option

--- guessing_game.py
+++ guessing_game.py
@@ -3,32 +3,37 @@
 from random import randint
 
 def start_game():
-    guesses = 0
+    print("***Number Guessing Game***")
     _min = 1
     _max = 50
-    print("***Number Guessing Game***")
-
     random_number = randint(_min, _max)
-    while True:
+    user_number = 0
+    guesses = 0
+
+    while random_number != user_number:
         try:
             user_number = int(input("Guess a number: "))
 
             if user_number < _min or user_number > _max: 
                 print("Input is out of range\n")
                 continue
+            if user_number > random_number: print("It's lower")
+            elif user_number < random_number: print("It's higher")
+            guesses += 1
 
-            if user_number == random_number: break
-            else:
-                if user_number > random_number: print("It's lower")
-                else: print("It's higher")
+            if user_number == random_number: 
+                print("You got it! The number was {}".format(random_number))
+                print("It took you {} guess(es)".format(guesses))
+                play_again = input("Play again? [Y/N]: ").lower()
+                if play_again == 'y': 
+                    random_number = randint(_min, _max)
+                    user_number = 0
+                    guesses = 0
+                    print()
+                    continue
         except ValueError: 
             print("Only integers are allowed\n")
             continue
-
-        guesses += 1
         print()
 
-    print("You got it! The number was {}".format(random_number))
-    print("It took you {} guess(es).".format(guesses))
-    
 if __name__ == '__main__': start_game()
